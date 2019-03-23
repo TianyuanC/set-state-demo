33e519a98919497a9e45473d592f63e494d09f06
Ryan Chu <tianyuan.chu@gmail.com>
2019-03-23 10:26:04 -0700

    examples

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    before = {
+        a: 1,
+        b: 2,
+        c: {
+            d: 1,
+            e: 2
+        }
+    };
+    state = {
+        a: 1,
+        b: 2,
+        c: {
+            d: 1,
+            e: 2
+        }
+    };
+    componentDidMount() {
+        console.log("update starts");
+
+        //batching
+        // this.setState({ a: 0 });
+        // this.setState({ b: 0 });
+        // this.setState({ a: 1 });
+
+        //shallow update
+        //this.setState({ a: 0 });
+        //this.setState({ c: { d: 2 } });
+        //this.setState(prevState => ({ c: { ...prevState.c, d: 2 } }));
+        console.log("update ends");
+    }
+
+    render() {
+        console.log("render");
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h2>Before</h2>
+                    <code>{JSON.stringify(this.before)}</code>
+                    <h2>After</h2>
+                    <code>{JSON.stringify(this.state)}</code>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
